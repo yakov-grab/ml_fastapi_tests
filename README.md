db22231fe855ffcfffb126426062a0a542d4bc2c
tokarevsas31 <alexander.tokarev@urfu.ru>
2023-03-13 13:24:03 +0500

    Added files

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# ml_fastapi_tests
+# An example of ML Application with the pretrained model and test.
+
+Пример для определения тональности текста на английском языке с помощью библиотеки Hugging Face
+
+Тестирует GitHub Actions
---
+++ main.py
@@ -0,0 +1,21 @@
+from fastapi import FastAPI
+from transformers import pipeline
+from pydantic import BaseModel
+
+
+class Item(BaseModel):
+    text: str
+
+
+app = FastAPI()
+classifier = pipeline("sentiment-analysis")
+
+
+@app.get("/")
+def root():
+    return {"message": "Hello World"}
+
+
+@app.post("/predict/")
+def predict(item: Item):
+    return classifier(item.text)[0]
