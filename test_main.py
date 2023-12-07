3c3a939f92752c62fdb502ea5cfa500c1ba7e8df
tokarevsas31 <90484184+tokarevsas31@users.noreply.github.com>
2023-12-07 18:26:40 +0500

    Update test_main.py

--- test_main.py
+++ test_main.py
@@ -7,7 +7,7 @@ client = TestClient(app)
 def test_read_main():
     response = client.get("/")
     assert response.status_code == 200
-    assert response.json() == {"message": "Hello World"}
+    assert response.json() == {"message": "World"}
 
 
 def test_predict_positive():
