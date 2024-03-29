ff82b717bc4cbcd1ccf2231d8729523b7e2ce3f1
yakov-grab <yakov.grab@yandex.ru>
2024-03-29 15:09:05 +0300

    add 2 new api methods and tests

--- main.py
+++ main.py
@@ -22,4 +22,19 @@ def predict(item: Item):
         result = classifier(item.text)[0]
         return result
     except Exception as e:
-        raise HTTPException(status_code=500, detail=f"Error processing text: {e}")
+        raise HTTPException(status_code=500,
+                            detail=f"Error processing text: {e}")
+
+
+@app.post("/translate/")
+def translate(item: Item, target_language: str):
+    translation_pipeline = pipeline("translation",
+                                    model="Helsinki-NLP/opus-mt-en-xx")
+    return translation_pipeline(item.text,
+                                target_language=target_language)
+
+
+@app.post("/keywords/")
+def keywords(item: Item):
+    keywords_pipeline = pipeline("keyword-extraction")
+    return keywords_pipeline(item.text)
--- test_main.py
+++ test_main.py
@@ -24,3 +24,23 @@ def test_predict_negative():
     json_data = response.json()
     assert response.status_code == 200
     assert json_data['label'] == 'NEGATIVE'
+
+
+def test_keywords():
+    text = "This is a test text. It contains some keywords\
+            like Python, FastAPI, and testing."
+    response = client.post("/keywords/",
+                           json={"text": text})
+    assert response.status_code == 200
+    assert isinstance(response.json(), list)
+    assert all(isinstance(keyword, str) for keyword in response.json())
+
+
+def test_translate():
+    text = "Hello!"
+    target_language = "fr"  # French
+    response = client.post("/translate/",
+                           json={"text": text,
+                                 "target_language": target_language})
+    assert response.status_code == 200
+    assert isinstance(response.json(), str)
